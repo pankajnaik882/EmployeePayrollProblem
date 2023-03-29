6e8d30034b2dfc8f8bfa461ed4a64bc3aa96a92e
“pankajnaik882” <pankaj080519999@gmail.com>
2023-03-29 13:53:29 +0530

    Add : Added Program.cs File

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace EmployeePayrollProblem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Please select given options");
+                Console.WriteLine("1.CreateConnectionsWithDatabase\n" +
+                    "8.Exit\n");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Console.Clear();
+                        EmployeeRepository.GetAllEmployees();
+                        Console.Write("\nPress any key to continue...... ");
+                        break;
+                    case 8:
+                        Console.Clear();
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Please select only given options");
+                        Console.Write("\nPress any key to continue...... ");
+                        break;
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
